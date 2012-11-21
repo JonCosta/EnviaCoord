52adc646eedc015b130b560e850cf222264e90a6
Jon <jonjcosta@gmail.com>
2012-11-20 22:58:20 -0200

    1st push

---
+++ gen/br/com/enviacoord/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package br.com.enviacoord;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/br/com/enviacoord/MainActivity.java
@@ -0,0 +1,120 @@
+package br.com.enviacoord;
+
+import java.util.HashMap;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import br.com.enviacoord.HttpClient;
+import br.com.enviacoord.R;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.Handler;
+import android.app.Activity;
+import android.content.Context;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends Activity implements LocationListener {
+
+	private Location local ;
+	private Handler handler ;
+	private boolean controle ; //Variável de controle de execução da Runnable
+	
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        TextView txtStatus = (TextView) findViewById(R.id.txtStatus) ;
+        txtStatus.setText(R.string.envioOn) ;
+        
+        handler = new Handler() ;
+        controle = true ;
+        //Cria novo objeto Runnable para ser executado em segundo plano
+        Runnable run = new Runnable(){
+        	public void run(){
+        		if(controle){
+        			mostrarCoord();
+        			handler.postDelayed(this, 5000) ;
+        		}
+        	} //Fecha run
+        }; //Fecha Runnable
+        handler.post(run) ;
+    }//Fecha onCreate
+    
+    //Método que cria objeto JSON, acessa a WS e retorna dados recebidos
+    public void mostrarCoord(){
+
+    	//Doubles para receber coordenadas do GPS
+    	double lat = 0, lng = 0 ; 
+    	String placa = "AKZ-0989" ;
+    	//HashMap que irá receber os parâmetros que serão covertidos em JSON
+    	HashMap params = new HashMap();
+    	//Cria o LocationManager
+    	LocationManager LM = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+    	LM.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+        String bestProvider = LM.getBestProvider(new Criteria(), true) ;
+        //Se obtém o local mais recente marcado pelo GPS
+        local = LM.getLastKnownLocation(bestProvider) ;
+        //Obtém-se os valores de latitude e longitude
+        lat = local.getLatitude() ;
+        lng = local.getLongitude();
+        //Valores são inseridos no HashMap
+        params.put("latitude", lat);
+    	params.put("longitude", lng);
+    	params.put("placa", placa) ;
+    	
+    	//Cria-se o objeto JSON a partir do HashMap
+        JSONObject jsonParams = new JSONObject(params);
+        //Obtém-se a 'reposta' da WebService, defindo o método a ser acessado e os parâmetros
+    	JSONObject resp = HttpClient.SendHttpPost(this.getString(R.string.url_ws), jsonParams);
+    	//Doubles para exibição das coordenadas
+    	double latExib = 0, lngExib =0;
+    	String placaExib = "" ;
+    	//Obtém-se como resposta da WS as mesmas coordenadas enviadas
+    	try {
+			latExib = resp.getDouble("coord1");
+			lngExib = resp.getDouble("coord2") ;
+			placaExib = resp.getString("placa") ;
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+    	//Exibe na tela a mensagem com as coordenadas recebidas da WS
+    	Toast.makeText(this, "Lat: "+latExib + " Lng:"+lngExib+ " Placa:" +placaExib, Toast.LENGTH_SHORT).show() ;
+    	
+    }//Fecha mostrarCoord
+    
+    /*MÉTODOS IMPLEMENTADOS PARA LOCATION LISTENER**/
+	@Override
+	public void onLocationChanged(Location location) {
+		this.local = location ;
+		
+	}
+
+	@Override
+	public void onProviderDisabled(String provider) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onProviderEnabled(String provider) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onStatusChanged(String provider, int status, Bundle extras) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	protected void onDestroy(){
+		super.onDestroy();
+		controle = false ;
+	}
+
+}
